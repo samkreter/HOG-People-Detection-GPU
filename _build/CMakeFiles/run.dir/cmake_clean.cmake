cdd7742e8baabe0ea305fd504df596609ff6f5a3
Sam Kreter <samkreter@gmail.com>
2016-04-25 15:48:52 -0500

    added previous work to git

---
+++ _build/CMakeFiles/run.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run.dir/src/test.cpp.o"
+  "run.pdb"
+  "run"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/run.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/test.cpp
@@ -0,0 +1,60 @@
+#include <opencv2/opencv.hpp>
+// #include "opencv2/highgui/highgui.hpp"
+// #include "opencv2/imgproc/imgproc.hpp"
+
+using namespace cv;
+using namespace std;
+
+int main (int argc, const char * argv[])
+{
+    VideoCapture cap;
+    cap.open("../peopleInMarket.mp4");
+    cap.set(CV_CAP_PROP_FRAME_WIDTH, 320);
+    cap.set(CV_CAP_PROP_FRAME_HEIGHT, 240);
+
+    if (!cap.isOpened())
+        return -1;
+
+    Mat img;
+    namedWindow("opencv", CV_WINDOW_AUTOSIZE);
+    HOGDescriptor hog;
+    hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
+
+    while (true){
+        cap >> img;
+
+        if (img.empty())
+            continue;
+
+        //resizing the video since it's so massive
+        resize(img, img, Size(640, 360), 0, 0, INTER_CUBIC);
+
+        vector<Rect> found, found_filtered;
+        hog.detectMultiScale(img, found, 0, Size(8,8), Size(32,32), 1.05, 2);
+        size_t i, j;
+        for (i=0; i<found.size(); i++)
+        {
+            Rect r = found[i];
+            for (j=0; j<found.size(); j++)
+                if (j!=i && (r & found[j]) == r)
+                    break;
+            if (j== found.size())
+                found_filtered.push_back(r);
+        }
+
+        for (i=0; i<found_filtered.size(); i++)
+        {
+            Rect r = found_filtered[i];
+            r.x += cvRound(r.width*0.1);
+        r.width = cvRound(r.width*0.8);
+        r.y += cvRound(r.height*0.07);
+        r.height = cvRound(r.height*0.8);
+        rectangle(img, r.tl(), r.br(), Scalar(0,255,0), 3);
+        }
+
+        imshow("opencv", img);
+        if (waitKey(10)>=0)
+            break;
+    }
+    return 0;
+}
