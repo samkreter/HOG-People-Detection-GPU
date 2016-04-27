dcf0db4a61d409394c164e0dc8c76cb927b3b420
Sam Kreter <samkreter@gmail.com>
2016-04-27 02:02:50 +0000

    adding image to view

--- src/CpuGpuHogtest.cpp
+++ src/CpuGpuHogtest.cpp
@@ -384,12 +384,11 @@ void App::run()
             else img = img_aux;
             img_to_show = img;
 
-            vector<Rect> found;
+            vector<Rect> found,found_filtered;
 
             // Perform HOG classification
             hogWorkBegin();
-            if (use_gpu)
-            {
+            if (use_gpu){
                 gpu_img.upload(img);
                 gpu_hog->setNumLevels(nlevels);
                 gpu_hog->setHitThreshold(hit_threshold);
@@ -398,20 +397,39 @@ void App::run()
                 gpu_hog->setGroupThreshold(gr_threshold);
                 gpu_hog->detectMultiScale(gpu_img, found);
             }
-            else
-            {
+            else{
                 cpu_hog.nlevels = nlevels;
                 cpu_hog.detectMultiScale(img, found, hit_threshold, win_stride,
                                          Size(0, 0), scale, gr_threshold);
             }
+
+            int j = 0, i = 0;
+            for (i=0; i<found.size(); i++){
+                Rect r = found[i];
+                for (j=0; j<found.size(); j++)
+                    if (j!=i && (r & found[j]) == r)
+                        break;
+                if (j== found.size())
+                    found_filtered.push_back(r);
+            }
+
+            for (int i=0; i<found_filtered.size(); i++){
+                Rect r = found_filtered[i];
+                r.x += cvRound(r.width*0.1);
+                r.width = cvRound(r.width*0.8);
+                r.y += cvRound(r.height*0.07);
+                r.height = cvRound(r.height*0.8);
+                rectangle(img_to_show, r.tl(), r.br(), Scalar(0,255,0), 3);
+            }
+
             hogWorkEnd();
 
             // Draw positive classified windows
-            for (size_t i = 0; i < found.size(); i++)
-            {
-                Rect r = found[i];
-                rectangle(img_to_show, r.tl(), r.br(), Scalar(0, 255, 0), 3);
-            }
+            // for (size_t i = 0; i < found.size(); i++)
+            // {
+            //     Rect r = found[i];
+            //     rectangle(img_to_show, r.tl(), r.br(), Scalar(0, 255, 0), 3);
+            // }
 
             if (use_gpu)
                 putText(img_to_show, "Mode: GPU", Point(5, 25), FONT_HERSHEY_SIMPLEX, 1., Scalar(255, 100, 0), 2);
