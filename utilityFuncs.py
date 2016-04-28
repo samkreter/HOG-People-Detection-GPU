5f902d11ddeef86f5579bf6c5d7d7348c4bd61d2
Sam Kreter <samkreter@gmail.com>
2016-04-27 22:20:40 -0500

    added graph images for final report

--- utilityFuncs.py
+++ utilityFuncs.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import pca
+
 
 imgSizes = [ 2, 4, 8, 10, 20]
 Ts = [ 1, 32, 128, 256, 512, 1024]
@@ -26,29 +26,54 @@ def takeAverageOfFile(filenameIn, filenameOut):
     np.savetxt(filenameOut,finalData,delimiter=",",fmt='%.5f')
     return finalData
 
-def getSumTimes(data):
-    return np.sum(data,axis=0)
+
 
 def getSumTimesFile():
     data = np.genfromtxt("times.csv",delimiter=',')
     return np.sum(data,axis=0)
 
-def getParalleSpeedUp(data):
-    for i in range(data[:,0].size):
-        data[i,3] = data[i,5] / data[i,2]
-    return data
+def plotSizeTime(data):
+    x = [ i for i in range(len(imgSizes))]
+    sizes = ["550x366","1100x732","2200x1464","2750x1830","5500x3660"]
+    gpu = []
+    cpu = []
+    for line in data[data[:,0] == 128]:
+        gpu.append(line[5])
+        cpu.append(line[4])
+    plt.plot(x,gpu,'go-',label="GPU")
+    plt.plot(x,cpu,'bo-',label="CPU")
+    plt.title("With Threads per Block set at 128")
+    plt.xticks(x,sizes)
+    plt.xlim([0,5])
+    plt.ylim([0,6.5])
+    plt.xlabel('Image Sizes')
+    plt.ylabel('Time to Compute (s)')
+    plt.grid(True)
+    plt.legend(loc=4)
+    fig1 = plt.gcf()
+    fig1.savefig('SizeTime.png')
+    plt.show()
 
-def plotPicSizeValues(data):
-    cs = ['r','b','g','y','k']
-    for c,picSize in zip(cs,picSizes):
-        plt.plot(data[data[:,1] == picSize,0], data[data[:,1] == picSize,2], c+'o-',label=str(picSize)+" Image Size")
+def plotThreadTime(data):
+    gpu = []
+    cpu = []
+    for line in data[data[:,1] == 10]:
+        gpu.append(line[5])
+        cpu.append(line[4])
+    plt.plot(Ts,gpu,'go-',label="GPU")
+    plt.plot(Ts,cpu,'bo-',label="CPU")
+    plt.title("With 2750x1830 Set Image")
+    plt.xticks(Ts,Ts)
     plt.xlabel('Number of Threads per Block')
-    plt.ylabel('Time to Process')
+    plt.ylabel('Time to Compute (s)')
+    plt.xlim([-1,1050])
     plt.grid(True)
     plt.legend()
-    fig1 = plt.gcf()
-    fig1.savefig('kValue.png')
+    # fig2 = plt.gcf()
+    # fig2.savefig('ThreadTime.png')
     plt.show()
+
+
 
 def plotTValues(data):
     cs = ['r','b','g','y','k','m']
@@ -77,6 +102,8 @@ def getLabels(data):
 
 if __name__ == '__main__':
     test = takeAverageOfFile("times.csv","averagedData.csv")
+    plotThreadTime(test)
+    #plotSizeTime(test)
     # test2 = test[test[:,2].argsort()]
     # getParalleSpeedUp(test)
     # final = test[test[:,2].argsort()]
